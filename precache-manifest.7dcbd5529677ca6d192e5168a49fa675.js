91ba8f2ad168c556b68513071c237782c1331719
folsmore <69353018+folsmore@users.noreply.github.com>
2020-08-28 20:27:00 +0500

    Add files via upload

---
+++ precache-manifest.7dcbd5529677ca6d192e5168a49fa675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e9638581bebb35371e604c3ff98237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c59598a28052b8e1e3cf",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b524ebd8980cbe029db9",
+    "url": "/static/js/2.56a0e5ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.56a0e5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59598a28052b8e1e3cf",
+    "url": "/static/js/main.dc17f349.chunk.js"
+  },
+  {
+    "revision": "7b181280bc025caa25a8",
+    "url": "/static/js/runtime-main.98085f26.js"
+  }
+]);
